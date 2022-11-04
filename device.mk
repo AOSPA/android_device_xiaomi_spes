96a99ba592c1f53c96e047f59e63e58721423650
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-11-04 07:47:08 +0530

    spes: props: Use 90hz frame_rate_multiple_threshold
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ie74af1b908144ec97a50ea6fd51a77ede9c50115

--- device.mk
+++ device.mk
@@ -210,7 +210,7 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    debug.sf.frame_rate_multiple_threshold=60 \
+    debug.sf.frame_rate_multiple_threshold=90 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.set_touch_timer_ms=200 \
