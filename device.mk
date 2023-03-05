1f2d74e8c75088708e5fd9e71b22aa2985b267ca
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-03-05 15:55:40 -0300

    Revert "spes: props: Disable getVsyncPeriodFromHWC"
    This reverts commit 034d6b1b877bc2781cef8a39f34acc68000d3d54.
    
    Change-Id: Idf3894230bbb4b56ff0ad753d79b96722513ff30

--- device.mk
+++ device.mk
@@ -276,9 +276,6 @@ PRODUCT_VENDOR_PROPERTIES += \
 # FM
 BOARD_HAVE_QCOM_FM := true
 
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sf.use_latest_hwc_vsync_period=0
-
 # FRP
 PRODUCT_VENDOR_PROPERTIES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
