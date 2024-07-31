0249f7043912bf9d21c2035d371f8e926c039754
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-07-31 20:28:34 +0000

    spes: Remove libhidltransport dependency in proprietary blobs
    Change-Id: I9041bea07efff352b1c477a30e02692972100bdf
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -17,6 +17,8 @@ function blob_fixup() {
         vendor/lib/android.hardware.camera.provider@2.4-legacy.so | vendor/lib64/android.hardware.camera.provider@2.4-legacy.so)
             grep -q "libcamera_provider_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_provider_shim.so" "${2}"
             ;;
+        vendor/bin/batterysecret | vendor/lib64/hw/fingerprint.fpc.default | vendor/lib64/libgoodixhwfingerprint | vendor/lib64/sensors.touch.detect)
+            "${PATCHELF}" --remove-needed libhidltransport.so "${2}"
     esac
 }
 
