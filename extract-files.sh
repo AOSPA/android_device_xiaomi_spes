97b98e480eb494a2a0c48a5dd2ce12b7eec76091
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-08-01 15:23:57 +0000

    spes: Remove libhwbinder dependency
    Change-Id: If33a9e6cee889ab4085a2e4909f20f9443c60902
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -20,6 +20,9 @@ function blob_fixup() {
         vendor/bin/batterysecret | vendor/lib64/hw/fingerprint.fpc.default)
             "${PATCHELF}" --remove-needed libhidltransport.so "${2}"
             ;;
+        vendor/bin/vendor/lib64/libgoodixhwfingerprint)
+            "${PATCHELF}" --remove-needed libhwbinder.so "${2}"
+            ;;
     esac
 }
 
