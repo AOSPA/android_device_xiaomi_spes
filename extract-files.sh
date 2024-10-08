a4cba079c86eadc4c51e9c97c39aaa0a2745f881
angelomds42 <angelomds42@gmail.com>
2024-10-07 21:53:30 -0300

    spes: Fix fingerprint.goodix.default soname with patchelf v0.17.2
    * When we try to fix it with ;FIX_SONAME the fingerprint breaks:
    
    E vndksupport: Could not load /vendor/lib64/hw/fingerprint.goodix.default.so from default namespace: dlopen failed: "/vendor/lib64/hw/fingerprint.goodix.default.so" loaded phdr 0x6cdf84c000 not in loadable segment.
    E HAL     : load: module=/vendor/lib64/hw/fingerprint.goodix.default.so
    E HAL     : unknown
    
    * So for now let's fix it this way
    
    Change-Id: I9be38f9036d8bc592c35714fc45426637be2eccc
    Signed-off-by: angelomds42 <angelomds42@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -24,6 +24,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
             ;;
+        vendor/lib64/hw/fingerprint.goodix.default.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --set-soname fingerprint.goodix.default.so "${2}"
+            ;;
         *)
             return 1
             ;;
