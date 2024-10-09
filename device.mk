38dbab95290c6e798ff99f48dba1e064faef3c78
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-10-09 21:15:38 +0000

    spes: Switch to AIDL fingerprint hal [1/2]
    Change-Id: I022833c52dbd738555eb696d823bdd90c2cffe47
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -68,13 +68,18 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     com.fingerprints.extension@1.0.vendor \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.hardware.fp.sideCap=true
+
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.fingerprint.type=side \
+    persist.vendor.fingerprint.sensor_location=930|150 \
+    persist.vendor.fingerprint.sensor_modules=fpc,goodix
 
 # Health
 TARGET_USE_HIDL_QTI_HEALTH := true
