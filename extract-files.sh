4df28925ac73f93e3bc494f3494016a54c6f45e1
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-10-11 22:07:53 +0000

    WIP
    Change-Id: Id700bfacb388baf6f427a53ceb601b5675a3c8c2
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 #
-# Copyright (C) 2022 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            [ "$2" = "" ] && return 0
             "${SIGSCAN}" -p "13 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib64/camera/components/com.qti.node.mialgocontrol.so)
+            [ "$2" = "" ] && return 0
             "${ANDROID_ROOT}"/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip --strip-debug "${2}"
             grep -q "libpiex_shim.so" "${2}" || ${PATCHELF} --add-needed "libpiex_shim.so" "${2}"
             ;;
         vendor/lib/android.hardware.camera.provider@2.4-legacy.so | vendor/lib64/android.hardware.camera.provider@2.4-legacy.so)
+            [ "$2" = "" ] && return 0
             grep -q "libcamera_provider_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_provider_shim.so" "${2}"
             ;;
         vendor/bin/batterysecret | vendor/lib/hw/*.so | vendor/lib/*.so | vendor/lib64/hw/*.so | vendor/lib64/*.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --remove-needed libhidltransport.so "${2}"
             "${PATCHELF}" --remove-needed libhwbinder.so "${2}"
             ;;
         vendor/etc/init/init.batterysecret.rc)
+            [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
             ;;
     esac
@@ -38,5 +43,6 @@ set -e
 export DEVICE=spes
 export DEVICE_COMMON=sm6225-common
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
