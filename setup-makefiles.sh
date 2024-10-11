8a5fe71148ee96ad5d464e1a6ed5b0e74fce328c
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-10-11 22:07:53 +0000

    WIP
    Change-Id: Id700bfacb388baf6f427a53ceb601b5675a3c8c2
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 #
-# Copyright (C) 2022 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1"
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
 set -e
 
 export DEVICE=spes
 export DEVICE_COMMON=sm6225-common
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
