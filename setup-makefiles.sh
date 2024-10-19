221c86eae12f906bf4efab08d50f58043c7df18c
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-10-19 14:29:24 +0000

    spes: Fix ELF check failures
    Change-Id: Ie11b68f1cdcd253162029655a5500112cb2e272e
    Co-authored-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -5,6 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function vendor_imports() {
+    cat << EOF >> "$1"
+        "device/xiaomi/spes",
+		"device/xiaomi/sm6225-common",
+		"hardware/xiaomi",
+EOF
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
